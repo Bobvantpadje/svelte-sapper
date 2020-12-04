8912f27e4d13e2bcb4b1a7abb98d9edb008be4e5
Bob Van 't Padje <bobvantpadje@Bobs-MacBook-Pro.local>
2020-12-04 14:10:21 +0100

    start on login styling

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,6 +1,6 @@
-
 <script lang="ts">
-  import { GoogleAuth } from '@beyonk/svelte-social-auth';
+  import { GoogleAuth } from "@beyonk/svelte-social-auth";
+  import Index from "./index.svelte";
 
   const YOUR_CLIENT_ID =
     "406460855468-59gcn6gkbruheij8gnosc6845lsil0a6.apps.googleusercontent.com";
@@ -9,13 +9,22 @@
   let email: string = "";
 </script>
 
-<div class="w-full min-h-screen flex">
-  <div class="w-1/2 bg-purple-900"></div>
-  <div class="w-1/2 flex flex-col align-middel justify-center text-center">
-    <h1>Welcome to passionate people</h1>
-    <h2>Please log in!</h2>
-    <input bind:value="{email}" placeholder="enter email" />
-    <button class="bg-red-400">login</button>
-    <GoogleAuth clientId="406460855468-59gcn6gkbruheij8gnosc6845lsil0a6.apps.googleusercontent.com" on:auth-success={e => console.dir(e.detail.user)} />
+<div class="w-full min-h-screen flex bg-purple-900 justify-center">
+  <div class="w-1/2 hidden lg:block"></div>
+  <div class="w-10/12 lg:w-1/2 lg:bg-white flex justify-center items-center">
+    <div
+      class="flex flex-col align-middel justify-center text-center bg-white p-8
+      rounded-lg max-w-sm"
+    >
+      <img src="logo-black-text.e1c73a1.svg" alt="logo" />
+      <h1>Welcome to passionate people</h1>
+      <h2>Please log in!</h2>
+      <input bind:value="{email}" placeholder="enter email" />
+      <button class="bg-red-400">login</button>
+      <GoogleAuth
+        clientId="406460855468-59gcn6gkbruheij8gnosc6845lsil0a6.apps.googleusercontent.com"
+        on:auth-success="{(e) => console.dir(e.detail.user)}"
+      />
+    </div>
   </div>
 </div>
